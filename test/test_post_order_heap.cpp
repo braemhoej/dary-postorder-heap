ab409978f2d01430fdfef2ec2e6f11b7d284b78d
Nicolaj Kuno Bræmhøj <nicolaj@braemhoej.me>
2022-04-02 21:45:06 +0200

    Additional tests

--- test/test_post_order_heap.cpp
+++ test/test_post_order_heap.cpp
@@ -6,42 +6,96 @@
 #include "gtest/gtest.h"
 #include "../src/postorder_heap.cpp"
 
-TEST(PostOrderHeap, TestPostOrderHeapCanPdddush) {
+TEST(PostOrderHeap, TestPushIncreasesSize) {
     auto comparator = [](int left, int right) {
         return left < right;
     };
     postorder_heap<int, std::vector<int>, decltype(comparator)> heap(comparator, 2);
-    for (int index = 0; index < 30; index++) {
-        heap.push(index);
+    for (int index = 0; index < 100; index++) {
+        EXPECT_EQ(heap.size(), index);
+        heap.push(rand());
+        EXPECT_EQ(heap.size(), index + 1);
+    }
+}
 
-    }
-    for (int index = 0; index < 30; index++)
-        std::cout << std::to_string(heap.poll()) << '\n';
-}
-TEST(PostOrderHeapTest, TestInsertAndPop1) {
+TEST(PostOrderHeap, TestPopDecreasesSize) {
     auto comparator = [](int left, int right) {
         return left < right;
     };
     postorder_heap<int, std::vector<int>, decltype(comparator)> heap(comparator, 2);
-    for (int index = 0; index<1000; index++) {
-        heap.push(index);
+    for (int index = 0; index < 100; index++) {
+        heap.push(rand());
     }
-    for (int index = 0;index < 1000; index++)
-        EXPECT_EQ(index, heap.poll());
+    for (int index = 100; index > 0; index--) {
+        EXPECT_EQ(heap.size(), index);
+        heap.pop();
+        EXPECT_EQ(heap.size(), index - 1);
+    }
 }
-TEST(PostOrderHeapTest, TestInsertAndPop2) {
+
+TEST(PostOrderHeapTest, TestTopDoesntDecreaseSize) {
     auto comparator = [](int left, int right) {
         return left < right;
     };
     postorder_heap<int, std::vector<int>, decltype(comparator)> heap(comparator, 2);
-    for (int index = 10000; index > 0; index--) {
+    for (int index = 0; index < 100; index++) {
+        heap.push(rand());
+    }
+    for (int index = 100; index > 0; index--) {
+        EXPECT_EQ(heap.size(), index);
+        int top = heap.top();
+        EXPECT_EQ(heap.size(), index);
+        heap.pop();
+    }
+}
+
+TEST(PostOrderHeap, TestEmptyIsTrueIfEmpty) {
+    auto comparator = [](int left, int right) {
+        return left < right;
+    };
+    postorder_heap<int, std::vector<int>, decltype(comparator)> heap(comparator, 2);
+    EXPECT_TRUE(heap.empty());
+    for (int index = 0; index < 100; index++) {
+        heap.push(rand());
+        EXPECT_FALSE(heap.empty());
+    }
+    EXPECT_FALSE(heap.empty());
+    for (int index = 100; index > 0; index--) {
+        EXPECT_FALSE(heap.empty());
+        heap.pop();
+    }
+    EXPECT_TRUE(heap.empty());
+}
+
+TEST(PostOrderHeapTest, TestSequentialTopExtractionsReturnDecreasingPriority) {
+    auto comparator = [](int left, int right) {
+        return left < right;
+    };
+    postorder_heap<int, std::vector<int>, decltype(comparator)> heap(comparator, 2);
+    for (int index = 0; index < 1000; index++) {
         heap.push(rand());
     }
     int last = std::numeric_limits<int>::min();
-    for (int index = 0; index < 10000; index++) {
-        int popped = heap.poll();
-        EXPECT_TRUE(last <= popped);
-        last = popped;
+    for (int index = 1000; index > 0; index--) {
+        int top = heap.top();
+        heap.pop();
+        EXPECT_TRUE(top >= last);
+        last = top;
     }
 }
-
+TEST(PostOrderHeapTest, TestSequentialTopExtractionsReturnDecreasingPriority2) {
+    auto comparator = [](int left, int right) {
+        return left > right;
+    };
+    postorder_heap<int, std::vector<int>, decltype(comparator)> heap(comparator, 2);
+    for (int index = 0; index < 1000000; index++) {
+        heap.push(rand());
+    }
+    int last = std::numeric_limits<int>::max();
+    for (int index = 1000000; index > 0; index--) {
+        int top = heap.top();
+        heap.pop();
+        EXPECT_TRUE(top <= last);
+        last = top;
+    }
+}
